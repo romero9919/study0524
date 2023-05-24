a330a56ba6f356f190aa2b3134f4f5a7acdf3030
Hojoong-Rome <hojoong9919@naver.com>
2023-05-24 17:27:34 +0900

    update

--- src/com/mycompany/study/ch02/HelloWorld.java
+++ src/com/mycompany/study/ch02/HelloWorld.java
@@ -8,6 +8,22 @@ public class HelloWorld {
 		System.out.println("25세");
 		System.out.println("경기도 고양시 일산동구 정발산동");
 		System.out.println("인간 성인 남성");
+		// 변수 선언 : 데이터타입 변수명;
+		// 변수 초기화 : 데이터타입 변수명 = 리터럴;
+		
+		boolean a = true;
+		boolean b = false;
+		
+		int c = 1;
+		int d;
+		d = 1;
+		
+		int e,f,g;
+		e = 10;
+		
+		System.out.println("c: " + c);
+		System.out.println("e: " + e);
+		
 	}
 
 }
