fc35234c796e70437e367b53916e0f7dd4104743
Hojoong-Rome <hojoong9919@naver.com>
2023-05-24 17:28:27 +0900

    update

---
+++ src/com/mycompany/study/ch02/Exc0524.java
@@ -0,0 +1,12 @@
+package com.mycompany.study.ch02;
+
+public class Exc0524 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int wow = 2;
+		
+		System.out.println(wow);
+	}
+
+}
