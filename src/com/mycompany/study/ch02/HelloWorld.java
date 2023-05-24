c8220f91fd27fd43d295625c2e542ce6597c430f
Hojoong-Rome <hojoong9919@naver.com>
2023-05-24 15:35:48 +0900

    update

---
+++ src/com/mycompany/study/ch02/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.mycompany.study.ch02;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+		System.out.println("이호중");
+		System.out.println("25세");
+		System.out.println("경기도 고양시 일산동구 정발산동");
+		System.out.println("인간 성인 남성");
+	}
+
+}
