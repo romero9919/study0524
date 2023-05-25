f7cf43ff79b51bb3809ce1e1c98e02c78b899528
Hojoong-Rome <hojoong9919@naver.com>
2023-05-25 16:59:14 +0900

    update

---
+++ src/com/mycompany/study/ch02/ExPromotion.java
@@ -0,0 +1,27 @@
+package com.mycompany.study.ch02;
+
+public class ExPromotion {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		byte byteVar = 10;
+		System.out.println(byteVar);
+		
+		short shortVar = byteVar;
+		System.out.println(shortVar);
+		
+		int intVar = shortVar;
+		System.out.println(intVar);
+		
+		long longVar = intVar;
+		System.out.println(longVar);
+		
+		float floatVar = longVar;
+		System.out.println(floatVar);
+		
+		double doubleVar = floatVar;
+		System.out.println(doubleVar);
+	}
+
+}
