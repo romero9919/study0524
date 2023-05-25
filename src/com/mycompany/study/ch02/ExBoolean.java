9b4eb01530ae91cb8ca1a5286872fa01d977809e
Hojoong-Rome <hojoong9919@naver.com>
2023-05-25 15:30:29 +0900

    update

---
+++ src/com/mycompany/study/ch02/ExBoolean.java
@@ -0,0 +1,28 @@
+package com.mycompany.study.ch02;
+
+public class ExBoolean {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// 선언
+		// 변수명은 defaultNy로 불리언 변수 선언
+		boolean defaultNy;
+		
+		// 초기화
+		// 변수명은 default1Ny로 불리언 변수 선언 초기값은 true
+		boolean default1Ny = true;
+		
+		boolean default2ny = false;
+		
+		// if-else문의 이해: 괄호 안이 참이면 if 구문 아니면 else 구문
+		if (default1Ny) {
+			System.out.println("기본값입니다.");
+		} else {
+			System.out.println("여분값입니다.");
+		
+		}
+		
+	}
+
+}
