4b32c3fd8f4a43a15467e857156a6eb6c6fb5c2f
Hojoong-Rome <hojoong9919@naver.com>
2023-05-25 17:34:02 +0900

    update

---
+++ src/com/mycompany/study/ch02/Exc0525.java
@@ -0,0 +1,33 @@
+package com.mycompany.study.ch02;
+
+public class Exc0525 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		boolean wow = true;
+		
+		byte oh = 11;
+		
+		char good = 'g';
+		
+		float man = 3;
+		
+		double dynamic = 3.14;
+		
+		int omg = 123;
+		
+		long amg = 12312431;
+		
+		long amazing = omg;
+		
+		System.out.println(wow);
+		System.out.println(oh);
+		System.out.println(good);
+		System.out.println(man);
+		System.out.println(dynamic);
+		System.out.println(omg);
+		System.out.println(amg);
+		System.out.println(amazing);
+	}
+
+}
