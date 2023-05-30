40b8b3ce91a46a5c43f32be61e3d79f32c749db4
Hojoong-Rome <hojoong9919@naver.com>
2023-05-30 17:07:57 +0900

    update

---
+++ src/com/mycompany/study/ch03/ExAssignmentOperator.java
@@ -0,0 +1,25 @@
+package com.mycompany.study.ch03;
+
+public class ExAssignmentOperator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// 대입 연산자(+=, -=, *=...)
+		
+		int result = 1;
+		int result2 = 2;
+		int result3 = 3;
+
+		
+		result += 10;		
+		System.out.println(result);
+		
+		result2 -= 10;
+		System.out.println(result2);
+		
+		result3 *= 10;
+		System.out.println(result3);
+	}
+
+}
--- src/com/mycompany/study/ch03/ExLogicalOperator.java
+++ src/com/mycompany/study/ch03/ExLogicalOperator.java
@@ -4,7 +4,25 @@ public class ExLogicalOperator {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		//논리 연산자
+		// && = &: and: 논리곱 :연산 참 참/참, 나머지 false
+		// || = |: or: 논라합: 연산 참 폴스/참, 폴스 참/참, 참 참/참, 폴스 폴스/폴스
+		// ^: 배타적 논리합: 연산 참 참/폴스, 폴스 폴스/폴스, 참 폴스/참, 폴스 참/참
+		
+		int a = 65;
+		
+		if(a >= 65 && a <= 70) {
+			System.out.println("&&: true");
+		}
+		
+		if(a > 65 || a <= 70) {
+			System.out.println("||: true");
+		}
+		
+		if(a > 65 ^ a <= 70) {
+			System.out.println("^: true");
+		}
 	}
 
 }
