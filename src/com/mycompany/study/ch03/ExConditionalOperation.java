2919249ffa326674bfd9ab6d6812f92e4d88fd3f
Hojoong-Rome <hojoong9919@naver.com>
2023-05-30 17:24:25 +0900

    update

--- src/com/mycompany/study/ch03/ExConditionalOperation.java
+++ src/com/mycompany/study/ch03/ExConditionalOperation.java
@@ -4,7 +4,19 @@ public class ExConditionalOperation {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		
+		int math = 85;
+		
+		char grade = (math > 90) ? 'A': 'B';
+		System.out.println("grade: " + grade);
+		
+		if(math > 90) {
+			grade = 'A';
+		} else {
+			grade = 'B';
+		}
+		System.out.println("grade: " + grade);
 	}
 
 }
