639f1e04685c273f9501dcdfb169ae41ec647929
Hojoong-Rome <hojoong9919@naver.com>
2023-05-31 17:39:29 +0900

    update

--- src/com/mycompany/study/ch04/ExIf3.java
+++ src/com/mycompany/study/ch04/ExIf3.java
@@ -4,7 +4,18 @@ public class ExIf3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		
+		int a = -56741;
+		
+		if (a == 0) {
+			System.out.println("0입니다.");
+		} else if (a % 2 == 1) {
+			System.out.println("입력하신 " + a + " = 홀수입니다.");
+		} else {
+			System.out.println("입력하신 " + a + " = 짝수입니다.");
+		}
+		
 	}
 
 }
