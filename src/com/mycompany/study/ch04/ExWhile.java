ce11da7c461491df7e81f0b743a2dc8ee42e5f7b
Hojoong-Rome <hojoong9919@naver.com>
2023-06-01 17:10:55 +0900

    update

---
+++ src/com/mycompany/study/ch04/ExWhile.java
@@ -0,0 +1,17 @@
+package com.mycompany.study.ch04;
+
+public class ExWhile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = 1;
+		int sum = 0;
+		
+		while(i<=10) {
+			sum += i;
+			System.out.println(sum);
+			i++;
+		}
+	}
+
+}
