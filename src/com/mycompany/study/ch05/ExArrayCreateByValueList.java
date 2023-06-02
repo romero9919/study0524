16e6cd885b4c5b7ae594e750d4af0242194d688b
Hojoong-Rome <hojoong9919@naver.com>
2023-06-02 14:59:01 +0900

    update

--- src/com/mycompany/study/ch05/ExArrayCreateByValueList.java
+++ src/com/mycompany/study/ch05/ExArrayCreateByValueList.java
@@ -4,7 +4,28 @@ public class ExArrayCreateByValueList {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		/*
+		double[] total;
+		double[] average;
 		
+		char[] grade2301;
+		char[] grade2302;
+		
+		boolean[] score;
+		boolean score2[];
+		*/
+		
+		int[] scoreChemistry = {100, 99, 98, 97, 9, 56, 56, 87};
+		String[] scoreStudent = {"Jongweon", "Pedro", "John", "Sunchan", "Xiaohu"};
+		char[] scoreGrade = {'A', 'B', 'C', 'D', 'E'};
+		
+		// int[] scorePhysics;
+		// scorePhysics = {100, 99, 98, 97, 96};
+		// error
+		
+		int a = scoreChemistry.length;
+		System.out.println(a);
+		System.out.println(scoreChemistry[0]);
 	}
 
 }
