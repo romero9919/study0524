7d11d4ac9572b95eb14475134fd60ecc8623c3a3
Hojoong-Rome <hojoong9919@naver.com>
2023-06-05 15:58:49 +0900

    update

---
+++ src/com/mycompany/study/ch05/ExEnumWeek.java
@@ -0,0 +1,17 @@
+package com.mycompany.study.ch05;
+
+public class ExEnumWeek {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=1;
+		Week week = null;
+		String abc;
+		
+		abc = Week.MONDAY.toString();
+	
+		System.out.print(abc);
+		
+	}
+
+}
---
+++ src/com/mycompany/study/ch05/ExRandom.java
@@ -0,0 +1,12 @@
+package com.mycompany.study.ch05;
+
+public class ExRandom {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println(Math.random());
+		
+	}
+
+}
