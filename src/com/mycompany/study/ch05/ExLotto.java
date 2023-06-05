2d1d6fa3f558fabeb347bcfb966ead168f6a4c25
Hojoong-Rome <hojoong9919@naver.com>
2023-06-05 16:23:57 +0900

    update

---
+++ src/com/mycompany/study/ch05/ExLotto.java
@@ -0,0 +1,10 @@
+package com.mycompany.study.ch05;
+
+public class ExLotto {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
--- src/com/mycompany/study/ch05/ExRandom.java
+++ src/com/mycompany/study/ch05/ExRandom.java
@@ -5,8 +5,19 @@ public class ExRandom {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println(Math.random());
+		/*
+		int min = 1;
+		int max = 10;
+		int random = (int)(Math.random()*(max-min) + min);
+		System.out.println(random);
+		*/
 		
+		
+		int min = 1;
+		int max = 46;
+		int random = (int)(Math.random()*(max-min) + min);
+		int random2 = (int)(Math.random()*(max-min) + min);
+		System.out.println(random + "+" + random2);
 	}
 
 }
