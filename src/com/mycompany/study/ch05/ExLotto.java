4e6790e682d5998882e6216dbd0628fbb19b9045
Hojoong-Rome <hojoong9919@naver.com>
2023-06-05 16:25:24 +0900

    update

--- src/com/mycompany/study/ch05/ExLotto.java
+++ src/com/mycompany/study/ch05/ExLotto.java
@@ -4,7 +4,13 @@ public class ExLotto {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		// 로또 번호 생성기
+		// 현재까지 나온 번호와 중복되지 않도록 함
+		// 현재까지 번호는 홈페이지에서 받을 것
+		// 중복 숫자 제거
+		// 3연속 번호 불가
+		// 보너스 번호는 제외
+		// 1070회 진행되었음
 	}
 
 }
