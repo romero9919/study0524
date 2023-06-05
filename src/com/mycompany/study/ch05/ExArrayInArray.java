31b57406ef63dac8e45cc0465604d13d690f9d05
Hojoong-Rome <hojoong9919@naver.com>
2023-06-05 15:07:04 +0900

    update

--- src/com/mycompany/study/ch05/ExArrayInArray.java
+++ src/com/mycompany/study/ch05/ExArrayInArray.java
@@ -4,6 +4,7 @@ public class ExArrayInArray {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		/*
 		int[][] score = {{55, 55, 55}, {78, 78, 78}, {68, 75, 45}, {88, 31, 15}, {54, 45, 21}};
 		
 		int sumKor = 0;
@@ -25,6 +26,38 @@ public class ExArrayInArray {
 					break;
 				}
 				System.out.println(sumKor + "|" + sumEng + "|" + sumMath);
+			}
+		}
+		
+		*/
+		/*
+		int one = 1;
+		for(int i=1; i<=9; i++) {
+			for(int j=1; j<=i; j++) {
+				System.out.print(j);
+				if(j==9) {
+					System.out.println(" ");
+					System.out.println("1 3 5 7 90");
+				}
+			}
+			System.out.println(" ");
+		}
+		*/
+		for(int i=1; i<=10; i++) {
+			if(i == 10) {
+				for(int j=1; j<=i-1; j++) {
+					if(j%2==1) {
+						System.out.print(j);
+					} else {
+						System.out.print(" ");
+					}
+				}
+				System.out.println("0");
+			} else {
+				for(int j=1; j<=i; j++) {
+					System.out.print(j);
+				}
+				System.out.println();
 			}
 		}
 		
---
+++ src/com/mycompany/study/ch05/Week.java
@@ -0,0 +1,11 @@
+package com.mycompany.study.ch05;
+
+public enum Week {
+	MONDAY,
+	TUESDAY,
+	WEDNESDAY,
+	THURSDAY,
+	FRIDAY,
+	SATURDAY,
+	SUNDAY
+}
