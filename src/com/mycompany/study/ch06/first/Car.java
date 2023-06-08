ea9451d5ec856b1260cc3608280b6d9c2c28f5a2
Hojoong-Rome <hojoong9919@naver.com>
2023-06-08 14:30:05 +0900

    update

---
+++ src/com/mycompany/study/ch06/first/Car.java
@@ -0,0 +1,20 @@
+package com.mycompany.study.ch06.first;
+
+public class Car {
+	
+	// field
+	int engine;
+	String name;
+	String color;
+	String company;
+	
+	// method
+	void go() {
+		System.out.println("앞으로 갑니다.");
+	}
+	
+	void back() {
+		System.out.println("뒤로 갑니다.");
+	}
+	
+}
