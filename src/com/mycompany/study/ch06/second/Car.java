388474108b6ea25ea8b6f071998c7d5b0aea9f3f
Hojoong-Rome <hojoong9919@naver.com>
2023-06-08 16:20:25 +0900

    update

--- src/com/mycompany/study/ch06/second/Car.java
+++ src/com/mycompany/study/ch06/second/Car.java
@@ -8,6 +8,18 @@ public class Car {
 	String color = "화이트";
 	String company = "현대자동차";
 	
+	
+	Car(String color){
+		this.color = color;
+	}
+	
+	Car(int engine, String name, String  color, String company){
+		this.engine = engine;
+		this.name = name;
+		this.color = color;
+		this.company = company;
+	}
+	
 	// method
 	void go() {
 		System.out.println("앞으로 갑니다.");
--- src/com/mycompany/study/ch06/second/ExCar.java
+++ src/com/mycompany/study/ch06/second/ExCar.java
@@ -4,7 +4,13 @@ public class ExCar {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		Car car = new Car();
+		
+		Car car2 = new Car("클릭");
+		
+		Car car3 = new Car(6, "M340i", "딥블루", "BMW");
+				
 	}
 
 }
