517c8b07de091aa2257a2006ac03dd90ee7502a7
Hojoong-Rome <hojoong9919@naver.com>
2023-06-09 14:32:33 +0900

    update

---
+++ src/com/mycompany/study/ch06/trfirst/Notice.java
@@ -0,0 +1,19 @@
+package com.mycompany.study.ch06.trfirst;
+
+import java.util.Date;
+
+public class Notice {
+	
+	
+	String header;
+	int count;
+	int view;
+	boolean isNew;
+	String writer;
+	Date regDate;
+	String contents;
+	
+	void alert() {
+		System.out.println("Notice");
+	}
+}
