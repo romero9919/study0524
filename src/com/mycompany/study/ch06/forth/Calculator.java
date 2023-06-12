de9d4b04742dc324ff2e8ed51d8d9b1ec5e91a42
Hojoong-Rome <hojoong9919@naver.com>
2023-06-12 14:24:45 +0900

    update

---
+++ src/com/mycompany/study/ch06/forth/Calculator.java
@@ -0,0 +1,12 @@
+package com.mycompany.study.ch06.forth;
+
+public class Calculator {
+	
+	public double areaSquare(double a) {
+		return a*a;
+	}
+	
+	public double areaSquare(double a, double b) {
+		return a*b;
+	}
+}
---
+++ src/com/mycompany/study/ch06/forth/ExCalculator.java
@@ -0,0 +1,10 @@
+package com.mycompany.study.ch06.forth;
+
+public class ExCalculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
