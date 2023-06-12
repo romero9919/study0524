39238e5e1f72cea2dcd0d63c9a6e5a4b4fd30c91
Hojoong-Rome <hojoong9919@naver.com>
2023-06-12 15:35:21 +0900

    update

---
+++ src/com/mycompany/study/ch06/eighth/ExEarth.java
@@ -0,0 +1,17 @@
+package com.mycompany.study.ch06.eighth;
+
+public class ExEarth {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		String buildName = "Burj Khalifa";
+		String mountName = "Everest";
+		
+		System.out.println(buildName + " " + Earth.TOP_BUILDING);
+		System.out.println(mountName + " " + Earth.TOP_MOUNTAIN);
+		
+	}
+
+}
---
+++ src/com/mycompany/study/ch06/seventh/ExPersion.java
@@ -0,0 +1,16 @@
+package com.mycompany.study.ch06.seventh;
+
+public class ExPersion {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		Persion persion = new Persion(null, "1234", "이호중");
+		
+//		persion.nation = "영국"; // final은 중간에 값 대입 불가능
+//		persion.ssn = "묻지마"; // final은 중간에 값 대입 불가능
+		persion.name = "Romero";
+	}
+
+}
