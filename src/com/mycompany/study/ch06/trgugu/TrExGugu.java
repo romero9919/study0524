a6709b620f9fce2b7d21a52299cd196a1dc4d0e6
Hojoong-Rome <hojoong9919@naver.com>
2023-06-12 16:09:25 +0900

    update

---
+++ src/com/mycompany/study/ch06/trgugu/TrExGugu.java
@@ -0,0 +1,16 @@
+package com.mycompany.study.ch06.trgugu;
+
+import java.util.Scanner;
+
+public class TrExGugu {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		int result = scanner.nextInt();
+		System.out.println(result);
+	}
+
+}
