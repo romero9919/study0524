0f05ef4ea36b8a6413492350ffd002c08941df1e
Hojoong-Rome <hojoong9919@naver.com>
2023-06-20 15:23:51 +0900

    update

--- src/com/mycompany/study/ch13/second/Car.java
+++ src/com/mycompany/study/ch13/second/Car.java
@@ -1,5 +1,13 @@
 package com.mycompany.study.ch13.second;
 
 public class Car {
-
+	
+	public static void main(String[] args) {
+		Product<String, Integer> product = new Product<>();
+		
+		product.setKind("Romero");
+		product.setModel(100);
+		
+		System.out.println(product.getKind() + product.getModel());
+	}
 }
