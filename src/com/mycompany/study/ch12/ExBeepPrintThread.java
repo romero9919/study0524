9370f555195453e60a3ce0133014039f12fc5f0d
Hojoong-Rome <hojoong9919@naver.com>
2023-06-20 09:42:08 +0900

    update

--- src/com/mycompany/study/ch12/ExBeepPrintThread.java
+++ src/com/mycompany/study/ch12/ExBeepPrintThread.java
@@ -1,13 +1,30 @@
 package com.mycompany.study.ch12;
 
+import java.awt.Toolkit;
+
 public class ExBeepPrintThread {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 //		//how1
-		Runnable runnable = new ExBeepRunnable();
-		Thread thread = new Thread(runnable);
+//		Runnable runnable = new ExBeepRunnable();
+//		Thread thread = new Thread(runnable);
 		
+//		//how2
+		Thread thread = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				Toolkit toolkit = Toolkit.getDefaultToolkit();
+				for(int i=0; i<5; i++) {
+					toolkit.beep();
+					try {
+						Thread.sleep(500);
+					} catch(Exception e) {
+						
+					}
+				}
+			}
+		});
 		thread.start();
 		
 		for(int i=0; i<5; i++) {
