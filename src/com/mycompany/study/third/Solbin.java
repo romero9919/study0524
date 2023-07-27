3c918ff32f75662d6050fc9eb3ffe59301b588c5
Hojoong-Rome <hojoong9919@naver.com>
2023-07-27 12:03:19 +0900

    update

---
+++ src/com/mycompany/study/third/Solbin.java
@@ -0,0 +1,14 @@
+package com.mycompany.study.third;
+
+public class Solbin {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		final String KIM_SOL_BIN = "KOKORO PYONG PYONG";
+		
+		System.out.println(KIM_SOL_BIN);
+		
+	}
+
+}
